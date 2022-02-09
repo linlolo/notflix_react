59077fcbb6a081fa1a53793b523cd429d52b94fd
Prasanna Thapa <thapapj19@gmail.com>
2022-02-09 10:17:25 -0500

    Added content.js page

---
+++ src/Pages/Content.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import React from "react";
+import {Link} from "react-router-dom";
+
+export default function Content() {
+    return (
+        <div className="content">
+            Pages
+        </div>
+    );
+}
